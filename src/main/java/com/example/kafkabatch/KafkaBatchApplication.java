ddee8f67946bbacf88b45073d56b76b50977ba24
moonseo.kim <moonseo.kim@nhn.com>
2021-08-03 13:47:05 +0900

    Init project

---
+++ src/main/java/com/example/kafkabatch/KafkaBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkabatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaBatchApplication.class, args);
+    }
+
+}
